740a3a4c9ba152d99ca75cfbd7a9272408890ae5
Nidhi Gupta <nidhiRains@nidhis-macbook-pro.local>
2016-03-06 18:16:54 +0530

    Initial Commit

---
+++ tableview assingment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tableview assingment
+//
+//  Created by Nidhi Gupta on 06/03/16.
+//  Copyright © 2016 Nidhi Gupta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
